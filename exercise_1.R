efc24e0311f7c05f464e4f9441f37ffa6338d68a
Iago Flávio <iagoflavio_sa@hotmail.com>
2024-06-18 15:55:02 -0300

    project initialize commit

---
+++ exercise_1.R
@@ -0,0 +1,11 @@
+# Tarefas:
+
+# Carregar os dados do arquivo CSV.
+# Filtrar as vendas realizadas no ano de 2023.
+# Calcular a receita total (Quantidade * Preço) para cada categoria de produto.
+# Ordenar as categorias pela receita total de forma decrescente.
+# Encontrar o produto mais vendido (em quantidade) no ano de 2023.
+# Calcular a média de preço dos produtos vendidos por categoria.
+# Determinar o mês com a maior receita total.
+# Adicionar uma coluna com o preço total da venda (Quantidade * Preço) e salvar o DataFrame atualizado em um novo CSV.
+# Salvar o resultado em um novo arquivo CSV.
